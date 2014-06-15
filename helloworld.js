b2f6952676b376a6ba9278b40df0ee037de8eac1
Elise Carmichael <ecarmichael@mobiquityinc.com>
2014-06-15 19:37:06 -0400

    Updating code to implement assignment 2

--- helloworld.js
+++ helloworld.js
@@ -1,13 +1,41 @@
 var fs = require('fs');
+var randomWords = require('random-words');
 
-var textToWriteToFile = "Hello World!"
-var logFile = "hello.log"
+var minWords = 1;
+var maxWords = 10;
+var logFile = "hello.log";
 
-fs.appendFile(logFile, textToWriteToFile + "\n", function (err) {
-    if(err) {
-        console.log("Error writing to file '" + logFile + "' - error is: " + err);
-    }
-    else {
-        console.log("Successfully wrote '" + textToWriteToFile + "' to file '" + logFile + "'");
-    }
+// Make a 'sentence'
+var textToWriteToFile = randomWords({ min: 1, max: 10, join: ' ' })
+
+// Update the log file
+WriteLineToFile(textToWriteToFile);
+
+// Log Status
+GetLinesInFile(function(numTimesRun) {
+    var ess = numTimesRun == 1 ? '' : 's';
+    console.log("This application has been run " + numTimesRun + " time" + ess + ".");
 });
+
+
+function GetLinesInFile(numLinesCallBack) {
+    fs.readFile(logFile, 'utf8', function (err, data) {
+      if (err) {
+        return console.log("Problem getting line count in file: " + err);
+      }
+      // there should be a \n at the end, so subtract 1
+      numLinesCallBack(data.split("\n").length - 1); 
+    });
+}
+
+function WriteLineToFile(lineToWrite) {
+    lineToWrite += "\n";
+    fs.appendFile(logFile, lineToWrite, function (err) {
+        if(err) {
+            console.log("Error writing to file '" + logFile + "' - error is: " + err);
+        }
+        else {
+            console.log("Successfully wrote '" + textToWriteToFile + "' to file '" + logFile + "'");
+        }
+    });   
+}
